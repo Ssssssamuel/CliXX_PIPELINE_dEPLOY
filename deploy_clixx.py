a696a8c10d03461861abbd39d420b7e834034646
unknown <samyfedyrob@yahoo.com>
2024-10-06 11:19:14 +0100

    Updated deploy_clixx file

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -11,7 +11,7 @@ credentials = assumed_role_object['Credentials']
 print("Assumed role credentials received.")
 
 # EC2 instance variables
-AWS_REGION = "us-east-1a"
+AWS_REGION = "us-east-1"
 KEY_PAIR_NAME = 'stack_devops_kp7.pem'
 AMI_ID = 'ami-00f251754ac5da7f0'  # Amazon Linux 2
 SUBNET_ID = 'subnet-0c6f53069ca4e9922' 
