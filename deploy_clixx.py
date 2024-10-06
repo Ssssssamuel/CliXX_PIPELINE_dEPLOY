9b3158e330af797890bba382d11300157f92262f
unknown <samyfedyrob@yahoo.com>
2024-10-06 06:01:11 +0100

    Updated deploy_clixx file

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -11,12 +11,6 @@ credentials=assumed_role_object['Credentials']
 
 print(credentials)
 
-# s3=boto3.client('s3',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
-# response = s3.list_buckets(
-#     MaxBuckets=123)
-
-# print(response)
-
 ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
 response = ec2.create_security_group(
     Description='My security group',
