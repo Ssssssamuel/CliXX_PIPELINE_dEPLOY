1d3d4e491faa476e3d9e5062c295932ff63f2107
unknown <samyfedyrob@yahoo.com>
2024-10-06 13:39:55 +0100

    Updated deploy_clixx file

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -15,7 +15,7 @@ print("Assumed role credentials received.")
 AWS_REGION = "us-east-1"
 KEY_PAIR_NAME = 'stack_devops_kp7'
 AMI_ID = 'ami-00f251754ac5da7f0'
-SUBNET_ID = '0c6f53069ca4e9922'
+SUBNET_ID = 'subnet-077c0abf304d257a5'
 SECURITY_GROUP_ID = 'sg-05048737fb0f14c99'
 #INSTANCE_PROFILE = 'EC2-Admin'
 
