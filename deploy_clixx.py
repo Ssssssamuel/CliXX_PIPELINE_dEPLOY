e2859d89427d39ca75ab5bb3a95d8d7f8f849395
unknown <samyfedyrob@yahoo.com>
2024-10-07 13:47:49 +0100

    Updated deploy_clixx file

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -118,6 +118,7 @@ sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
 
 # Updating WordPress to recognize client session
 sudo sed -i "s|/\* That's all, stop editing! \*/|if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') { \$_SERVER['HTTPS'] = 'on'; }\n/* That's all, stop editing! */|" /var/www/html/wp-config.php
+
  
 # Grant file ownership of /var/www & its contents to apache user
 sudo chown -R apache /var/www
